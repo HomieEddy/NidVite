df775ab9c82d48f4b55ad3c6f81b31344530fc56
HomieEddy <mceddy95@hotmail.fr>
2026-05-05 20:04:46 -0400

    feat(email): add bilingual Markdown email template for status updates

---
+++ resources/views/emails/report-status-updated.blade.php
@@ -0,0 +1,29 @@
+@php($locale = $locale ?? 'fr')
+
+<x-mail::message>
+# {{ __('email.status_updated.greeting', ['uuid' => $report->uuid], $locale) }}
+
+{{ __('email.status_updated.body', [
+    'old' => __("status.{$oldStatus}", [], $locale),
+    'new' => __("status.{$report->status}", [], $locale),
+], $locale) }}
+
+@if($report->status === 'rejected' && $report->rejection_reason)
+**{{ __('email.status_updated.rejection_reason', [], $locale) }}:** {{ $report->rejection_reason }}
+@endif
+
+<x-mail::panel>
+**{{ __('tracking.Numéro', [], $locale) }}:** {{ $report->uuid }}<br>
+**{{ __('tracking.Date', [], $locale) }}:** {{ $report->created_at->translatedFormat('j F Y') }}<br>
+**{{ __('tracking.Statut actuel', [], $locale) }}:** {{ __("status.{$report->status}", [], $locale) }}
+</x-mail::panel>
+
+<x-mail::button :url="route('report.tracking', $report->uuid)">
+{{ __('email.status_updated.track_button', [], $locale) }}
+</x-mail::button>
+
+{{ __('email.status_updated.footer', [], $locale) }}
+
+{{ __('email.status_updated.signature', [], $locale) }}<br>
+**{{ config('app.name') }}**
+</x-mail::message>
