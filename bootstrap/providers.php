2a01b1cc1aa1d81145e60ceb1eaf17bd35f1c8af
HomieEddy <mceddy95@hotmail.fr>
2026-05-05 20:56:24 -0400

    feat(reverb): publish broadcasting config and register BroadcastServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,10 +5,12 @@ use App\Providers\AuthServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
 use App\Providers\FortifyServiceProvider;
 use App\Providers\TelescopeServiceProvider;
+use Illuminate\Broadcasting\BroadcastServiceProvider;
 
 return [
     AppServiceProvider::class,
     AuthServiceProvider::class,
+    BroadcastServiceProvider::class,
     AdminPanelProvider::class,
     FortifyServiceProvider::class,
     TelescopeServiceProvider::class,
