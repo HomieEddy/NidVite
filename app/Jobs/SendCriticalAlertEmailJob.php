4aa19308c27f9b21cf29c892ef4f37d010c52703
HomieEddy <mceddy95@hotmail.fr>
2026-05-11 19:24:57 -0400

    fix(ci): remove conflicting afterCommit property on queued job

--- app/Jobs/SendCriticalAlertEmailJob.php
+++ app/Jobs/SendCriticalAlertEmailJob.php
@@ -18,8 +18,6 @@ class SendCriticalAlertEmailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public bool $afterCommit = true;
-
     public int $tries = 3;
 
     public function __construct(
