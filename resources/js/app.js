e826eaf5091586a0929dd28928982d07bc625a1b
HomieEddy <mceddy95@hotmail.fr>
2026-05-10 23:24:43 -0400

    fix(map): allow nominatim CSP and bundle Leaflet marker icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,18 @@
 import './bootstrap';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 window.L = L;
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+	iconRetinaUrl: markerIcon2x,
+	iconUrl: markerIcon,
+	shadowUrl: markerShadow,
+});
 
 window.nidviteTracker = function nidviteTracker(errorMsg) {
 	return {
