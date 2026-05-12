c6bcb08004c86dcfabf902f2d623f32eaf7c0367
HomieEddy <mceddy95@hotmail.fr>
2026-05-12 13:24:22 -0400

    style(phase-28): fix Pint formatting for code-quality gate

--- app/Http/Controllers/ReportTrackingController.php
+++ app/Http/Controllers/ReportTrackingController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Report;
 use App\Models\ReportFollower;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
